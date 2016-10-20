38784c98abb965ad0340b15282a24959d4237c53
Pachakamakk <etienne.normand@epitech.eu>
2016-10-20 12:46:53 +0200

    \#12: Adding simple socket.io handling with simple behaviour

--- controllers/real-time.js
+++ controllers/real-time.js
@@ -1,5 +1,12 @@
 const algorithm = require('../algorithm');
+const io = require('../libraries/socket-io.js'); //Not sure about syntax...
 
-// Example, DELETE ME
-module.exports.foo = function(app) {
+module.exports.start = function(app) {
+    //Create socket.io instance
+    io.on('connection', function (socket) {
+	socket.on('data_stream', function (value1, value2) {
+	    // Do logging of data}
+	});
+    });
+    //Start c++ side
 }
