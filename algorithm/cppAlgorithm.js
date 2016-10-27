0f407ab23639b4a8c013eeb80f43f8e28a444e26
Pachakamakk <etienne.normand@epitech.eu>
2016-10-27 12:58:25 +0200

    \#12: Clean up silly code

--- algorithm/cppAlgorithm.js
+++ algorithm/cppAlgorithm.js
@@ -3,5 +3,5 @@ const algoGen = require('../cpp/build/Release/algoGen.node');
 // Example, DELETE ME
 
 module.exports.foo = function(app) {
-    console.log(algoGen.launchSimulation(10, 2);
+    console.log(algoGen.launchSimulation(10, 2));
 }
--- controllers/real-time.js
+++ controllers/real-time.js
@@ -1,5 +1,6 @@
 const algorithm = require('../algorithm');
-const io = require('../libraries/socket-io.js'); //Not sure about syntax...
+//const io = require('../libraries/socket-io.js'); //Not sure about syntax...
+var io = require('socket.io')(80);
 
 module.exports.start = function(app) {
     //Create socket.io instance
