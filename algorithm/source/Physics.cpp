fec256073f2fe533b44c99aceb50f90c9030b351
Nicolas Chauvin <chauvin.nico@gmail.com>
2016-11-03 17:11:35 +0100

    #36: Add Physical world access methods

--- algorithm/source/Physics.cpp
+++ algorithm/source/Physics.cpp
@@ -1,10 +1,36 @@
 #include "Physics.hpp"
-#include "VrepAPI.hpp"
 
 Physics::Physics()
+  : _worldHandle(-1)
 {
 }
 
 Physics::~Physics()
 {
+}
+
+bool Physics::enterTheMatrix()
+{
+  return vrep::start("localhost", 19997) != -1;
+}
+
+void Physics::getOffTheMatrix()
+{
+  vrep::finish(_worldHandle);
+}
+
+bool Physics::startSimulation()
+{
+  return vrep::startSimulation(_worldHandle) == 0;
+}
+
+bool Physics::endSimulation()
+{
+  return vrep::stopSimulation(_worldHandle) == 0;
+}
+
+Physics::Object *Physics::getObject(const std::string &name)
+{
+  (void)name;
+  return NULL;
 }
--- algorithm/source/Physics.hpp
+++ algorithm/source/Physics.hpp
@@ -1,13 +1,28 @@
 #ifndef		PHYSICS_HPP_
 # define	PHYSICS_HPP_
 
+# include <map>
+# include <string>
+# include "VrepAPI.hpp"
+
 class		Physics
 {
 public:
   class Object;
 
+private:
+  vrep::handle_t _worldHandle;
+  std::map<std::string, Object *> _objects;
+
+public:
   Physics();
   ~Physics();
+
+  bool enterTheMatrix();
+  void getOffTheMatrix();
+  bool startSimulation();
+  bool endSimulation();
+  Object *getObject(const std::string &);
 };
 
 #endif		/* !PHYSICS_HPP_ */
