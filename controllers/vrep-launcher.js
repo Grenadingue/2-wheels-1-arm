f7991aec07ed9d106b0594d475a95ccb445aff7f
cristi_a <toma.cristini@gmail.com>
2016-11-16 15:27:45 +0100

    #55 : Keeping child process

--- controllers/vrep-launcher.js
+++ controllers/vrep-launcher.js
@@ -1,24 +1,36 @@
 const config = require('../config/base.json');
-var sys = require('sys')
-var exec = require('child_process').exec;
+var exec = require('child_process').spawn;
 
 module.exports.launch = function(instance_number) {
    var path = process.argv.length > 2 ? process.argv[2] : config.vrep_path;
-   var port_list = [];
-
-   function puts(error, stdout, stderr) {
-      console.log("<------- error ------->")
-      console.log(error);
-      console.log("<------- stdout ------->")
-      console.log(stdout);
-      console.log("<------- stderr ------->")
-      console.log(stderr);
-   }
+   var internalData = [];
 
    function launchOneInstance(path, inputed_port) {
       console.log(path + " -h -gREMOTEAPISERVERSERVICE_" + inputed_port + "_TRUE_FALSE -s10000 -q 2w1a.ttt");
-      exec(path + " -h -gREMOTEAPISERVERSERVICE_" + inputed_port + "_TRUE_FALSE -s10000 -q 2w1a.ttt", puts);
-      port_list.push(port);
+
+      var new_process = exec(path, [
+            "-h",
+            "-gREMOTEAPISERVERSERVICE_" + inputed_port + "_TRUE_FALSE",
+            "-s10000",
+            "-q 2w1a.ttt"]);
+
+      new_process.stdout.on('data', function(data) {
+         console.log(inputed_port + ' stdout -------> ${data}');
+         console.log(data);
+      });
+      new_process.stderr.on('data', function(data) {
+         console.log(inputed_port + ' stderr ------->');
+         console.log(data);
+      });
+
+      new_process.on('exit', (code) => {
+        console.log(`Child exited with code ${code}`);
+      });
+
+      internalData.push({
+         port: inputed_port,
+         process: new_process
+      });
    }
 
    var port = 19997;
@@ -26,5 +38,5 @@ module.exports.launch = function(instance_number) {
       launchOneInstance(path, port);
       port += 10;
    }
-   return port_list;
+   return internalData;
 }
