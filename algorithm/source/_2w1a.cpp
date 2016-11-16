da02f8a68e85a92ffa504fb124f3ee01a4dfa7d8
Pachakamakk <etienne.normand@epitech.eu>
2016-11-16 16:51:39 +0100

    #62: Add isMoving() method and waitWhileMoving() in _2w1a class. Timeout to be implemented

--- algorithm/source/_2w1a.cpp
+++ algorithm/source/_2w1a.cpp
@@ -1,4 +1,5 @@
 #include "_2w1a.hpp"
+#include <iostream>
 
 _2w1a::_2w1a(const Physics::Object &robot,
 	     Physics::Object &wrist, Physics::Object &elbow, Physics::Object &shoulder)
@@ -12,6 +13,23 @@ _2w1a::_2w1a(const Physics::Object &robot,
 _2w1a::~_2w1a()
 {
 }
+
+bool	_2w1a::checkDifference(vrep::position_t &currentPosition) const
+{
+  float	totalOld = 0;
+  float	totalCurrent = 0;
+  float buffer = 0;
+  
+  totalOld = _lastPosition.x + _lastPosition.y + _lastPosition.z;
+  totalCurrent = currentPosition.x + currentPosition.y + currentPosition.z;
+  buffer = totalCurrent - totalOld;
+  if (buffer < 0)
+    buffer *= -1;
+  if (buffer > 0.0015)
+    return true;
+  return false;
+}
+
 Physics::Articulation &_2w1a::wrist()
 {
   return _wrist;
@@ -25,4 +43,30 @@ Physics::Articulation &_2w1a::elbow()
 Physics::Articulation &_2w1a::shoulder()
 {
   return _shoulder;
+}
+
+bool	_2w1a::isMoving()
+{
+  vrep::position_t	currentPosition;
+  
+  if (!getPositionOnMap(currentPosition))
+    return false;
+  if (_firstMove == true)
+    {
+      _lastPosition = currentPosition;
+      _firstMove = false;
+      return true;
+    }
+  if (checkDifference(currentPosition))
+    {
+      _lastPosition = currentPosition;
+      return true;
+    }
+  return false;
+}
+
+void	_2w1a::waitWhileMoving()
+{
+  while (isMoving())
+    std::this_thread::sleep_for(std::chrono::milliseconds(600));
 }
--- algorithm/source/_2w1a.hpp
+++ algorithm/source/_2w1a.hpp
@@ -1,6 +1,8 @@
 #ifndef		_2W1A_HPP_
 # define	_2W1A_HPP_
 
+# include <chrono>
+# include <thread>
 # include "Object.hpp"
 # include "Articulation.hpp"
 
@@ -10,14 +12,23 @@ private:
   Physics::Articulation &_wrist;
   Physics::Articulation &_elbow;
   Physics::Articulation &_shoulder;
-
+  vrep::position_t	_lastPosition;
+  bool			_firstMove;
+  
 public:
   _2w1a(const Physics::Object &, Physics::Object &, Physics::Object &, Physics::Object &);
   virtual ~_2w1a();
 
+protected:
+  bool	checkDifference(vrep::position_t &) const;
+
+public:  
   Physics::Articulation &wrist();
   Physics::Articulation &elbow();
   Physics::Articulation &shoulder();
+
+  bool isMoving();
+  void waitWhileMoving();
 };
 
 #endif		/* !_2W1A_HPP_ */
